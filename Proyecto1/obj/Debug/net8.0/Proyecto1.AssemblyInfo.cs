640ddb07c5629e0114c573cb3f2902a3e2793ff7
Gaston Gutierrez Llomplat <gastong7337@gmail.com>
2025-05-20 16:14:29 -0300

    Branch CalcV2

--- Proyecto1/obj/Debug/net8.0/Proyecto1.AssemblyInfo.cs
+++ Proyecto1/obj/Debug/net8.0/Proyecto1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e71e531c3ed8cb11de118274f4260a5a5dd6b04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef2cb773aeb3fc8a32a383d847d74efd06432572")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
